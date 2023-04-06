003f50ec3619f393ba375c037bc4807088dbb9c9
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-04-06 21:05:04 +0900

    Chore: recoil,react-query,tailwind css 셋팅
    index.tsx에 RecoilRoot,QueryClientProvider 설정
    App.css 추가 후 tailwind 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {QueryClient, QueryClientProvider} from "react-query";
+import {RecoilRoot} from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const queryClient = new QueryClient({
+    defaultOptions:{
+        queries:{
+            staleTime: 5 * 60 * 1000
+        }
+    }
+})
 root.render(
   <React.StrictMode>
-    <App />
+      <QueryClientProvider client={queryClient}>
+         <RecoilRoot>
+             <App />
+         </RecoilRoot>
+      </QueryClientProvider>
   </React.StrictMode>
 );
 
