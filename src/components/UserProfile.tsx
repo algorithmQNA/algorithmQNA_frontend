90a0423c6cff7a23a695fc09e4b16d29fb1f819a
이진희 <49019236+bebusl@users.noreply.github.com>
2023-04-08 10:00:25 +0900

    Feat: UserProfile 컴포넌트 구현
    * 유저 정보 간략히 나타내는 컴포넌트
    * 뱃지 파일 형식/파일 이름에 따라 badges props type 변경 필요

---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Rounded from "./RoundedImage";
+
+type UserProfileProps = {
+  username: string;
+  profileImgSrc: string;
+  badges: { name: string; src: string }[];
+};
+//TODO : 배지 레벨별 이미지 다 만들어서 할 지 아이콘 가져와서 배경색만 바꿀지에 따라 badges type 바꿔주기
+
+function UserProfile({ username, profileImgSrc, badges }: UserProfileProps) {
+  return (
+    <div className="grid grid-rows-2 auto-cols-max grid-flow-col items-center gap-x-4	">
+      <div className="row-span-2 col-span-1">
+        <Rounded alt={`${username}s profile image`} src={profileImgSrc} />
+      </div>
+      <div className="col-span-2">
+        <p className="text-sm">{username}</p>
+      </div>
+      <div className="row-span-1 col-span-2 flex flex-row gap-1">
+        {/* 임시 데이터 */}
+        {badges.map((badge) => (
+          <Rounded alt={badge.name} src={badge.src} width="16px" />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default UserProfile;
