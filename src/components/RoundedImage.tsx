c319e605708a23e3bef21efb795a9dc9c18b8834
이진희 <49019236+bebusl@users.noreply.github.com>
2023-04-08 09:59:02 +0900

    Feat: RoundedImage 컴포넌트 구현
    * 이미지 동그라미로 크롭해주는 컴포넌트

---
+++ src/components/RoundedImage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import type { ImgHTMLAttributes } from "react";
+
+type RoundedImageProps = ImgHTMLAttributes<HTMLImageElement> & {
+  alt: string;
+};
+
+function Rounded(props: RoundedImageProps) {
+  return <img className="rounded-full" {...props} />;
+}
+
+export default Rounded;
