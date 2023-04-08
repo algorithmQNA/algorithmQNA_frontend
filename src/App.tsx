10f10fce73d54dcd815d294d28ca23678c6478fe
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-04-08 19:04:25 +0900

    Feat: component 구현
    DefaultLayout,DropDown,Input,Button,TableRow 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './App.css'
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
 
-function App() {
+function App(){
+
   return (
+<<<<<<< Updated upstream
       <div className="App w-[500px]">
+=======
+      <div className="App">
+          <Header/>
+          <div className={'h-[2000px]'}>
+
+          </div>
+          <Footer/>
+>>>>>>> Stashed changes
       </div>
   );
 }
