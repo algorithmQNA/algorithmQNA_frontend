8b92228c76985650d744181df24e061978f9ab37
이진희 <49019236+bebusl@users.noreply.github.com>
2023-04-08 09:57:32 +0900

    Feat: Paper 컴포넌트 구현
    * props로 width, height 받아서 조정 가능
    * presentational component

---
+++ src/components/Paper.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import type { ReactNode } from "react";
+
+type PaperProps = {
+  width?: string;
+  height?: string;
+  children: ReactNode;
+};
+
+function Paper({ width = "300px", height = "300px", children }: PaperProps) {
+  return (
+    <div
+      className="shadow-md rounded text-center p-3"
+      style={{ width, height }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Paper;
