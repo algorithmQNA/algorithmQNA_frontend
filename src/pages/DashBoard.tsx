51421cf428939983627cdf80797336877c8101ba
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-04-10 22:00:48 +0900

    Design: 컴포넌트 그림자 추가

--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -20,14 +20,14 @@ import RightIcon from "../components/Icon/Right";
 // </section>
 export default function DashBoardPage(){
     return(
-        <div>
+        <div className={'pb-12'}>
             <div className={'min-h-[400px]'}>
                 <div className={'flex items-center'}>
 
                 </div>
             </div>
             <div className={'container m-auto grid grid-cols-1 xl:grid-cols-3 h-full md:gap-5'}>
-                <section className={'col-span-1 py-4 px-2 md:p-4 xl:p-5 bg-primary md:mt-4'}>
+                <section className={'col-span-1 py-4 px-2 md:p-4 xl:p-5 bg-primary md:mt-4 c-shadow'}>
                     <div className={'flex items-center justify-between py-3 px-4'}>
                         <h2 className={'font-bold text-2xl text-white'}>Q&N 게시판</h2>
                         <span className={'text-white flex gap-2 items-center text-sm'}>
@@ -43,7 +43,7 @@ export default function DashBoardPage(){
                         <TableRow img={'/logo512.png'} title={'포스트 제목'} writer={'작성자'} date={'2020-10-20 11:12:31'} view={1000} comment={1000}/>
                     </div>
                 </section>
-                <section className={'col-span-1 py-4 px-2 md:p-4 xl:p-5 bg-primary md:mt-4'}>
+                <section className={'col-span-1 py-4 px-2 md:p-4 xl:p-5 bg-primary md:mt-4 c-shadow'}>
                     <div className={'flex items-center justify-between py-3 px-4'}>
                         <h2 className={'font-bold text-2xl text-white'}>꿀팁 게시판</h2>
                         <span className={'text-white flex gap-2 items-center text-sm'}>
@@ -59,7 +59,7 @@ export default function DashBoardPage(){
                         <TableRow img={'/logo512.png'} title={'포스트 제목'} writer={'작성자'} date={'2020-10-20 11:12:31'} view={1000} comment={1000}/>
                     </div>
                 </section>
-                <section className={'col-span-1 py-4 px-2 md:p-4 xl:p-5 bg-primary md:mt-4'}>
+                <section className={'col-span-1 py-4 px-2 md:p-4 xl:p-5 bg-primary md:mt-4 c-shadow'}>
                     <div className={'flex items-center justify-between py-3 px-4'}>
                         <h2 className={'font-bold text-2xl text-white'}>미채택 글</h2>
                         <span className={'text-white flex gap-2 items-center text-sm'}>
