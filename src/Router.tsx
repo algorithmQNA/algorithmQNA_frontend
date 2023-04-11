d93f59d6811765ed84bb72d70fbdc502d711dbcb
이진희 <49019236+bebusl@users.noreply.github.com>
2023-04-11 22:15:57 +0900

    Feat: Router 임시 작성

---
+++ src/Router.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import * as Admin from "./pages/Admin";
+import * as MyPage from "./pages/MyPage";
+import SidebarLayout from "./components/layouts/SidebarLayout";
+
+const adminPages = [
+  { link: "notice", title: "공지사항" },
+  { link: "post", title: "게시판 글 관리" },
+];
+
+const mypagePages = [
+  { link: "profile", title: "내 정보" },
+  { link: "post", title: "내가 쓴 글" },
+];
+
+function Router() {
+  return (
+    <Routes>
+      <Route index element={<div>메인</div>} />
+      <Route path="admin" element={<SidebarLayout links={adminPages} />}>
+        <Route index element={<Navigate to="post" />} />
+        <Route path="post" element={<Admin.Post />} />
+        <Route path="notice" element={<Admin.Notice />} />
+      </Route>
+      <Route path="mypage" element={<SidebarLayout links={mypagePages} />}>
+        <Route index element={<Navigate to="profile" />} />
+        <Route path="profile" element={<MyPage.Profile />} />
+        <Route path="post" element={<MyPage.Post />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default Router;
