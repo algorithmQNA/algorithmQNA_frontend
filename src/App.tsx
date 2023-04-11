897fb0198d49fa6e1d2c790c66467be397ac5c08
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-04-11 20:38:40 +0900

    Design: 슬라이드 추가
    대시보드 임시 완료

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import DashBoardPage from "./pages/DashBoard";
 function App(){
   return (
       <div className="App">
-          <header className={'sticky bg-[#77A4E8] top-0 shadow-lg'}>
+          <header className={'sticky bg-[#77A4E8] top-0 shadow-lg z-50'}>
               <Header/>
           </header>
           <main className={'bg-[#F5F5F5]'}>
--- src/pages/DashBoard.tsx
+++ src/pages/DashBoard.tsx
@@ -1,32 +1,30 @@
-import Paper from "../components/Paper/Paper";
+
 import TableRow from "../components/TableRow/TableRow";
-import {Link} from "react-router-dom";
 import RightIcon from "../components/Icon/Right";
-// section className={'col-span-1 py-4 px-2 md:p-4 xl:p-5 bg-primary md:mt-4 md:rounded-lg'}>
-//     <div className={'flex items-center justify-between py-3 px-4'}>
-//     <h2 className={'font-bold text-2xl text-white'}>Q&N 게시판</h2>
-// <span className={'text-white flex gap-2 items-center text-sm'}>
-//                             전체 보기
-//                             <RightIcon/>
-//                         </span>
-// </div>
-// <div className={'flex flex-col gap-2'}>
-//     <TableRow img={'/logo512.png'} title={'포스트 제목'} writer={'작성자'} date={'2020-10-20 11:12:31'} view={1000} comment={1000}/>
-//     <TableRow img={'/logo512.png'} title={'포스트 제목'} writer={'작성자'} date={'2020-10-20 11:12:31'} view={1000} comment={1000}/>
-//     <TableRow img={'/logo512.png'} title={'포스트 제목'} writer={'작성자'} date={'2020-10-20 11:12:31'} view={1000} comment={1000}/>
-//     <TableRow img={'/logo512.png'} title={'포스트 제목'} writer={'작성자'} date={'2020-10-20 11:12:31'} view={1000} comment={1000}/>
-//     <TableRow img={'/logo512.png'} title={'포스트 제목'} writer={'작성자'} date={'2020-10-20 11:12:31'} view={1000} comment={1000}/>
-// </div>
-// </section>
+import {Swiper,SwiperSlide} from "swiper/react";
+import {Scrollbar} from "swiper";
+
+import "swiper/swiper.css";
+import "swiper/css/scrollbar";
+
 export default function DashBoardPage(){
     return(
         <div className={'pb-12'}>
-            <div className={'min-h-[400px]'}>
-                <div className={'flex items-center'}>
-
-                </div>
+            <div className={'h-[400px]'}>
+                <Swiper
+                    scrollbar={{
+                        hide: true,
+                    }}
+                    modules={[Scrollbar]}
+                    className="mySwiper w-full h-full"
+                >
+                    <SwiperSlide className={'w-full h-full bg-blue-300'}>
+                    </SwiperSlide>
+                    <SwiperSlide className={'w-full h-full bg-blue-400'}>
+                    </SwiperSlide>
+                </Swiper>
             </div>
-            <div className={'container m-auto grid grid-cols-1 xl:grid-cols-3 h-full p-3 sm:p-0 gap-3 md:gap-5'}>
+            <div className={'container m-auto grid grid-cols-1 xl:grid-cols-3 h-full p-3 sm:p-0 gap-3 md:gap-5 mt-6'}>
                 <section className={'col-span-1 md:mt-4 bg-white rounded-xl'}>
                     <div className={'flex items-center justify-between py-4 px-6 dashboard-title rounded-t-xl'}>
                         <h2 className={'font-bold text-2xl'}>Q&N 게시판</h2>
