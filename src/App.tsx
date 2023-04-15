f771136ac562fbb344fba8f264a50c2564b8ff1c
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-04-15 19:09:12 +0900

    Feat: CKEditor 모듈 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import QNABoard from "./pages/Q&ABoard/Q&ABoard";
+import PostWritePage from "./pages/PostWrite";
 function App(){
   return (
       <div className="App">
@@ -9,7 +9,7 @@ function App(){
               <Header/>
           </header>
           <main className={'bg-[#F5F5F5]'}>
-              <QNABoard/>
+              <PostWritePage/>
           </main>
           <footer className={'flex justify-between items-center p-2 w-full border-t border-t-[#D9D9D9]'}>
               <Footer/>
