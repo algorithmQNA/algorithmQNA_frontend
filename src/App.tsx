75d6d09ff1435da042fd7cdfd00c6356bd257326
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-04-16 13:01:09 +0900

    Refactor: 불필요 파일 및 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,13 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Pagination from "./components/Pagination/Pagination";
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
-import {useEffect, useRef} from "react";
-import GoogleLoginButton from "./components/oauth/GoogleLogin";
 
 function App(){
-
-
   return (
       <div className="App">
           <header className={'sticky bg-[#77A4E8] top-0 shadow-lg z-50'}>
               <Header/>
           </header>
-          <GoogleLoginButton/>
           <main className={'bg-[#F5F5F5]'}>
               <Router>
                   <Routes>
