97d21aa134750678d29d88f1f020b19f8ba2ae52
이진희 <49019236+bebusl@users.noreply.github.com>
2023-04-18 18:16:45 +0900

    Fix: 병합충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import './App.css'
+import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import DefaultRouter from "./DefaultRouter";
 
-function App(){
+function App() {
   return (
-      <div className="App">
-          <header className={'sticky bg-[#77A4E8] top-0 shadow-lg z-50'}>
-              <Header/>
-          </header>
-          <main className={'bg-[#F5F5F5]'}>
-              <Router>
-                  <Routes>
-                  </Routes>
-              </Router>
-          </main>
-          <footer className={'flex justify-between items-center p-2 w-full border-t border-t-[#D9D9D9]'}>
-              <Footer/>
-          </footer>
-      </div>
+    <div className="App">
+      <header className={"sticky bg-[#77A4E8] top-0 shadow-lg z-50"}>
+        <Header />
+      </header>
+      <main className={"bg-[#F5F5F5]"}>
+        <Router>
+          <DefaultRouter />
+        </Router>
+      </main>
+      <footer
+        className={
+          "flex justify-between items-center p-2 w-full border-t border-t-[#D9D9D9]"
+        }
+      >
+        <Footer />
+      </footer>
+    </div>
   );
 }
 export default App;
