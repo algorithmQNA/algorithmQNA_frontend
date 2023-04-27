822c6418e415b4d0cfa4170a14ec8043013d99da
이진희 <49019236+bebusl@users.noreply.github.com>
2023-04-27 23:03:48 +0900

    Chore: msw 브라우저 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { RecoilRoot } from "recoil";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+
+if (process.env.NODE_ENV === 'development') {
+    const { worker } = require('./mocks/browser');
+    worker.start({ quiet: true });
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 5 * 60 * 1000,
+    defaultOptions: {
+        queries: {
+            staleTime: 5 * 60 * 1000,
+        },
     },
-  },
 });
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <App />
-      </RecoilRoot>
-    </QueryClientProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RecoilRoot>
+                <App />
+            </RecoilRoot>
+        </QueryClientProvider>
+    </React.StrictMode>
 );
