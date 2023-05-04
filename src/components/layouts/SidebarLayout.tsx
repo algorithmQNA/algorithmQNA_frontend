45c789a6bbe997b4e1f6c2401fd5d79ce7ec4785
이진희 <49019236+bebusl@users.noreply.github.com>
2023-05-04 15:19:54 +0900

    Refactor: 사이드바 메뉴 코드 정리

--- src/components/layouts/SidebarLayout.tsx
+++ src/components/layouts/SidebarLayout.tsx
@@ -1,7 +1,5 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-
-import { ReactComponent as Hamburger } from '../../assets/images/Hamburger.svg';
 
 type SidebarLayoutProps = {
   links: { title: string; link: string }[];
@@ -9,38 +7,27 @@ type SidebarLayoutProps = {
 };
 
 function SidebarLayout({ links }: SidebarLayoutProps) {
-  const [openSidebar, setOpenSidebar] = useState(false);
-  const [title, setTitle] = useState('');
-
-  const toggleOpenSidebar = () => {
-    setOpenSidebar((prev) => !prev);
-  };
-
   /** 모바일 환경에서 헤더/사이드바 제외한 부분 클릭 시 사이드바 닫히도록 */
-  const handleClickMain = () => {
-    setOpenSidebar(false);
-  };
-
   return (
-    <div className="mx-auto my-0 max-w-[1200px]">
-      <div className="min-h-[1000px] bg-white flex flex-wrap md:flex-nowrap">
+    <div className="mx-auto my-4 max-w-[1200px]">
+      <div className="min-h-[1000px] bg-white flex flex-wrap md:flex-nowrap gap-4">
         <aside className="w-full sm:w-48 transition-transform">
-          <menu className="h-full p-2 overflow-y-auto bg-white border-gray-100 border-y border-r">
-            <p className="text-sm font-semibold">메뉴</p>
-            <ul className="before:content-normal before:block before:bg-orange-400 before:w-7 before:h-px before:my-1">
+          <menu className="p-2 overflow-y-auto bg-white border-border border">
+            <p aria-label="메뉴 이름" className="text-sm font-semibold">
+              메뉴
+            </p>
+            <ul className="before:content-normal before:block before:bg-secondary before:w-7 before:h-px before:my-1">
               {links.map((link) => (
                 <li key={link.title}>
                   <NavLink
+                    role="menuitem"
                     to={link.link}
                     className={({ isActive }) => {
                       const defaultStyle =
                         'block w-full h-full ps-3 py-2 hover:text-gray-800 transition-colors duration-200 text-xs';
-                      if (isActive) {
-                        // setTitle(link.title);
-                        console.log('IS ACTIVE,', link.link, isActive);
-                        return `${defaultStyle} text-gray-800`;
-                      }
-                      return `${defaultStyle} text-gray-500`;
+                      return `${defaultStyle} ${
+                        isActive ? 'text-gray-950' : 'text-gray-400'
+                      }`;
                     }}
                   >
                     {link.title}
@@ -50,10 +37,7 @@ function SidebarLayout({ links }: SidebarLayoutProps) {
             </ul>
           </menu>
         </aside>
-        <main
-          className="p-4 border-gray-100 border-y flex-grow"
-          onClick={handleClickMain}
-        >
+        <main className="p-4 border-border border flex-grow">
           <Outlet />
         </main>
       </div>
