cb9bd4f21abe556064885dfcd7dccc4a46a67c12
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-05-09 20:52:47 +0900

    Fix: 페이지 보기 추천 비추천 기능 수정

--- src/components/Recommend/RecoomendButton.tsx
+++ src/components/Recommend/RecoomendButton.tsx
@@ -5,7 +5,7 @@ import './style.css'
 export function RecommendBtn(){
     return(
         <label className={'rec-btn'}>
-            <input type={'radio'} className={'hidden'} name={'rec'}/>
+            <input type={'checkbox'} className={'hidden'} name={'rec'}/>
             <div className={'flex flex-col border-blue-500 border-2 py-1 px-6 rounded-lg text-blue-500'}>
                 <FiThumbsUp size={24}/>
                 <span className={'font-bold text-center'}>12</span>
@@ -16,7 +16,7 @@ export function RecommendBtn(){
 export function UnRecommendBtn(){
     return(
         <label className={'unrec-btn'}>
-            <input type={'radio'} className={'hidden'} name={'rec'}/>
+            <input type={'checkbox'} className={'hidden'} name={'rec'}/>
             <div className={'flex flex-col border-red-500 border-2 py-1 px-6 rounded-lg text-red-500'}>
                 <FiThumbsDown size={24}/>
                 <span className={'font-bold text-center'}>12</span>
