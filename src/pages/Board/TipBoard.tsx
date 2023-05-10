be309b147a0a49ae82aa28f6e3f9d1723c179a21
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-05-10 20:37:55 +0900

    Feat: 팁 게시판 추가

--- src/pages/Board/TipBoard.tsx
+++ src/pages/Board/TipBoard.tsx
@@ -1,14 +1,12 @@
-import './style.css'
-import Pagination from "../../components/Pagination/Pagination";
-import {SelectBox, SelectOption} from "../../components/DropDown/SelectBox";
-import PostTableRow from "../../components/TableRow/PostTableRow";
-import AnnouncementTableRow from "../../components/TableRow/Announcement";
 import PageTitle from "../../components/PageTitle/PageTitle";
 import CategoryBar from "../../components/Board/CategoryBar";
-import {useState} from "react";
+import {SelectBox, SelectOption} from "../../components/DropDown/SelectBox";
+import AnnouncementTableRow from "../../components/TableRow/Announcement";
+import PostTableRow from "../../components/TableRow/PostTableRow";
+import Pagination from "../../components/Pagination/Pagination";
 import RowListTo from "../../components/Board/ListTo";
 
-export default function QNABoardPage(){
+export default function TipBoardPage(){
     const list = [
         {name:'Brute Force',id:'1'},
         {name:'TWO_POINTER',id:'2'},
@@ -24,16 +22,16 @@ export default function QNABoardPage(){
     return(
         <div>
             <PageTitle>
-                질문 & 답변 게시판
+                꿀팁 게시판
             </PageTitle>
             <div className={'main-content p-4'}>
                 <div className={'grid lg:grid-cols-7 gap-6'}>
                     <div className={'col-span-2 flex flex-col gap-6'}>
-                        <CategoryBar name={'알고리즘'} list={list}/>
+                        <CategoryBar name={'알고리즘'} list={list} />
                     </div>
                     <div className={'col-span-5 flex flex-col gap-6'}>
                         <div className={'border border-[#D9D9D9] bg-transparent py-4 px-6 rounded flex items-center justify-between'}>
-                            <div className={'w-full max-w-[125px]'}>
+                            <div className={'w-full max-w-[175px]'}>
                                 <SelectBox>
                                     <SelectOption>최신순</SelectOption>
                                     <SelectOption>오래된순</SelectOption>
