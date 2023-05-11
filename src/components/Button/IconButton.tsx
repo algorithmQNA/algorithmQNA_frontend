7cca9f6aa32188e390137d027c2ad365784072a4
이진희 <49019236+bebusl@users.noreply.github.com>
2023-05-11 16:37:43 +0900

    Refactor: IconButton 사용법 수정

--- src/components/Button/IconButton.tsx
+++ src/components/Button/IconButton.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React from 'react';
 
 type IconButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
   Icon: JSX.Element;
   text?: string;
+  children?: string | JSX.Element | React.ReactNode;
 };
 
 function IconButton({ children, Icon, text, ...props }: IconButtonProps) {
   return (
-    <button className="rounded-lg hover:bg-slate-200 p-1 m-0" {...props}>
+    <button
+      className="rounded-lg hover:bg-slate-200 p-1 m-0 text-xs text-gray-700 p-0 m-0"
+      {...props}
+    >
       {Icon}
-      {text && <span className="text-xs text-gray-700 p-0 m-0">{text}</span>}
+      {children}
     </button>
   );
 }
