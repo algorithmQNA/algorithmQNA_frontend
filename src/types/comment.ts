91e4c87ddcc20d619d478b26b54f556d9807279b
이진희 <49019236+bebusl@users.noreply.github.com>
2023-05-13 09:00:21 +0900

    Feat: comment관련 신규 API 호출 함수 작성

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -16,6 +16,23 @@ export type Comment = {
   isLiked: boolean;
 };
 
+/**comment 형식 물어보고 적용 */
+export type EachComment = {
+  commentId: number;
+  memberId: number;
+  memberName: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  likeCnt: number;
+  dislikeCnt: number;
+  hasChild: boolean;
+  depth: 0 | 1 | 2 | 3;
+  isPinned: boolean;
+  childCommentList?: EachComment[];
+  childSize?: number;
+};
+
 export type ReportedComment = {
   reportCommentId: number;
   commentId: number;
