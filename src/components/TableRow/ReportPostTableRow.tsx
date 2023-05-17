b75d30fcf7abe3f367743e9a9cd9ba08acf9e2dd
이진희 <49019236+bebusl@users.noreply.github.com>
2023-05-17 17:36:22 +0900

    Feat: 신고내역 UI 구현

--- src/components/TableRow/ReportPostTableRow.tsx
+++ src/components/TableRow/ReportPostTableRow.tsx
@@ -10,225 +10,107 @@ import { AiFillDelete } from 'react-icons/ai';
 import useModal from '../../hooks/useModal';
 import Modal from '../Modal/Modal';
 
-import { queryClient } from '../..';
-import { useMutation } from 'react-query';
-import { deleteNotification } from '../../apis/adminApi';
 import ButtonComponent from '../Button/ButtonComponent';
 import CommentView from '../CommentView/Comment';
+import { Member } from '../../types/member';
+import { getReportedPostDetailRequest } from '../../apis/adminApi';
+import { useQuery } from 'react-query';
+import { Report, ReportConst } from '../../constants/Report';
+import UserProfile from '../UserProfile/UserProfile';
 
 interface AdminPageTableRowProps {
   title?: string;
-  date?: string;
+  date: string;
   id?: number;
+  member: Member;
 }
 export default function ReportPostTableRow({
-  title = '이거 어케 푸나요?',
-  date = '2023-05-07 23:04:11',
-  id = 2001,
+  title,
+  date,
+  id,
 }: AdminPageTableRowProps) {
   const navigate = useNavigate();
   const { open, closeModal, openModal } = useModal();
-  // const { mutate } = useMutation({
-  //   mutationFn: deleteNotification,
-  //   onSuccess: () => {
-  //     // TODO : notice query auto refetch
-  //     queryClient.invalidateQueries(["notification"]);
-  //   },
-  // });
+  const { open: deleteConfirmModal, closeModal: closeDeleteConfirmModal } =
+    useModal();
+  const { data } = useQuery(['table'], () => getReportedPostDetailRequest(10));
 
   /** 삭제 Confirm 모달 관련 함수들 */
   const handleClickViewButton = () => {
     openModal();
   };
 
-  const handleConfirm = () => {
-    // 삭제 API 요청.
-    //mutate(id);
-    closeModal();
-  };
-
-  const handleModifyBtnClick = () => {
-    navigate('/write');
-  };
-
-  const mockReportedPost = {
-    postId: 3,
-    memberId: 1,
-    memberName: '욕설러',
-    memberRole: '일반 유저',
-    content: '<p>이런 X발</p>',
-    PostReportList: [
-      {
-        reportPostId: 1,
-        memberId: 10,
-        memberName: '신고자1',
-        category: 'EEEEE',
-        detail: '욕했어요',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 2,
-        memberId: 10,
-        memberName: '신고자2',
-        category: 'EEEEE',
-        detail: '욕했어요',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 3,
-        memberId: 10,
-        memberName: '신고자3',
-        category: 'EEEEE',
-        detail: '욕했어요',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 4,
-        memberId: 10,
-        memberName: '신고자4',
-        category: 'EEEEE',
-        detail: '욕했어요3',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 5,
-        memberId: 10,
-        memberName: '신고자5',
-        category: 'EEEEE',
-        detail: '욕했어요5',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 5,
-        memberId: 10,
-        memberName: '신고자5',
-        category: 'EEEEE',
-        detail: '욕했어요5',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 5,
-        memberId: 10,
-        memberName: '신고자5',
-        category: 'EEEEE',
-        detail: '욕했어요5',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 5,
-        memberId: 10,
-        memberName: '신고자5',
-        category: 'EEEEE',
-        detail: '욕했어요5',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 5,
-        memberId: 10,
-        memberName: '신고자5',
-        category: 'EEEEE',
-        detail: '욕했어요5',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 5,
-        memberId: 10,
-        memberName: '신고자5',
-        category: 'EEEEE',
-        detail: '욕했어요5',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 5,
-        memberId: 10,
-        memberName: '신고자5',
-        category: 'EEEEE',
-        detail: '욕했어요5',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 5,
-        memberId: 10,
-        memberName: '신고자5',
-        category: 'EEEEE',
-        detail: '욕했어요5',
-        updatedAt: '2020-03-20',
-      },
-      {
-        reportPostId: 5,
-        memberId: 10,
-        memberName: '신고자5',
-        category: 'EEEEE',
-        detail: '욕했어요5',
-        updatedAt: '2020-03-20',
-      },
-    ],
-  };
-
-  return (
-    <>
-      {open && (
-        <Modal onClose={closeModal}>
-          <div className="flex justify-stretch w-full">
-            <section className="flex-grow">
-              <CommentView
-                commentId={mockReportedPost.postId}
-                content={mockReportedPost.content}
-                memberName="김솔민"
-                createdAt="2023-01-10"
-                depth={0}
-                isLiked={false}
-                dislikeCount={0}
-                isPinned={false}
-                likeCount={0}
-                memberCommentBadge={0}
-                memberId={1}
-                memberLikeBadge={0}
-                memberPostBadge={0}
-                memberProfile=""
-                parentId={10}
-              />
-            </section>
-            <section className="flex-grow">
-              <ul>
-                {mockReportedPost.PostReportList.map((report) => {
-                  return (
-                    <li>
-                      <ul>
-                        <li>{report.memberName}</li>
-                        <li>{report.category}</li>
-                        <li>{report.detail}</li>
-                      </ul>
-                    </li>
-                  );
-                })}
-              </ul>
-            </section>
-          </div>
-        </Modal>
-      )}
-      <div className={`p-4 gap-1.5 bg-box-bg border-border relative`}>
-        <div className={'flex flex-row justify-between h-full w-full'}>
-          <div>
-            <span className={'font-bold text-sm truncate max-w-[200px]'}>
-              {title}
-            </span>
-            <div className={'flex items-center justify-between w-full text-xs'}>
-              <div>
-                <BiTimeFive style={{ display: 'inline' }} />
-                <span className={'text-[#9ca3af] text-xs text-right'}>
-                  {setDateWritten(date)}
-                </span>
+  if (data?.data)
+    return (
+      <>
+        {open && (
+          <Modal title={`${title} 신고내역`} onClose={closeModal}>
+            <div className="flex justify-stretch w-full h-[32rem] overflow-auto">
+              <section className="flex-grow basis-1/2 h-full overflow-auto">
+                <div className="flex justify-between bg-box-bg p-2 border border-border">
+                  <UserProfile {...data.data.member} />
+                  <div className="flex flex-row items-end"></div>
+                </div>
+                <div
+                  className="ck ck-content p-2 text-left"
+                  dangerouslySetInnerHTML={{
+                    __html: data.data.content,
+                  }}
+                />
+              </section>
+              <section className="flex-grow basis-1/2 h-full overflow-auto">
+                <div className="bg-box-bg">
+                  {data.data.PostReports.map((report, idx) => {
+                    return (
+                      <div className="w-full border text-left p-2">
+                        <div className="w-full flex flex-row justify-between">
+                          <div className="font-medium">
+                            {report.member.memberName}
+                          </div>
+                          <div className="rounded-lg bg-red-500 text-sm text-white px-2">
+                            {Report[report.category]}
+                          </div>
+                        </div>
+                        {idx % 2 === 0 && (
+                          <div className="mt-2 font-light">기타사유 detail</div>
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+                <div>
+                  <ButtonComponent>n개의 신고글 삭제</ButtonComponent>
+                  <ButtonComponent>게시글 삭제</ButtonComponent>
+                </div>
+              </section>
+            </div>
+          </Modal>
+        )}
+        <div className={`p-4 gap-1.5 bg-box-bg border-border relative`}>
+          <div className={'flex flex-row justify-between h-full w-full'}>
+            <div>
+              <span className={'font-bold text-sm truncate max-w-[200px]'}>
+                {title}
+              </span>
+              <div
+                className={'flex items-center justify-between w-full text-xs'}
+              >
+                <div>
+                  <BiTimeFive style={{ display: 'inline' }} />
+                  <span className={'text-[#9ca3af] text-xs text-right'}>
+                    {setDateWritten(date)}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="text-right absolute bottom-4 right-4">
-            <ButtonComponent onClick={handleClickViewButton}>
-              신고 사유 보기
-            </ButtonComponent>
+            <div className="text-right absolute bottom-4 right-4">
+              <ButtonComponent onClick={handleClickViewButton}>
+                신고 사유 보기
+              </ButtonComponent>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  return <div>No Data</div>;
 }
