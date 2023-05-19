cf53a0db1e71a22d0848f3e264d5996a5cb24b16
이진희 <49019236+bebusl@users.noreply.github.com>
2023-05-19 17:47:11 +0900

    Feat: API 스펙 변경에 따른 mock 서버 수정

--- src/constants/Report.ts
+++ src/constants/Report.ts
@@ -9,13 +9,13 @@ export enum Report {
   ETC,
 }
 
-export const ReportConst={
-  SLANG:"비속어",
-  POLITICAL:"정치적 발언",
-  AD:"광고 등 상업적인 목적",
-  INSULT:"모욕",
-  LUSSTFUL: "음란",
-  OUT_OF_TOPIC : '주제를 벗어남',
-  OUT_OF_FORMAT:'게시판 형식에 맞지 않음',
-  ETC:"기타사유"
-}
+export const ReportConst = {
+  SLANG: '비속어',
+  POLITICAL: '정치적 발언',
+  AD: '광고 등 상업적인 목적',
+  INSULT: '모욕',
+  LUSSTFUL: '음란',
+  OUT_OF_TOPIC: '주제를 벗어남',
+  OUT_OF_FORMAT: '게시판 형식에 맞지 않음',
+  ETC: '기타사유',
+} as const;
