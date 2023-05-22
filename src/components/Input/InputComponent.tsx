e61f5f863d88e98d8c8a5e481911d28721842666
tnd10327 <104749358+tnd10327@users.noreply.github.com>
2023-05-22 21:59:46 +0900

    Design: 버튼 컴포넌트 수정
    및 버튼 수정에 따른 버그 수정

--- src/components/Input/InputComponent.tsx
+++ src/components/Input/InputComponent.tsx
@@ -1,12 +1,7 @@
-import React, { ButtonHTMLAttributes, ChangeEvent, ReactElement } from "react";
-import ButtonComponent from "../Button/ButtonComponent";
+import React from "react";
 
 interface props extends React.HTMLAttributes<HTMLInputElement> {
   value: string;
-  button?: {
-    children: string | ReactElement | ReactElement[];
-    event?: () => void;
-  };
 }
 /**
  * input.value = 저장 변수
@@ -17,7 +12,6 @@ export default function InputComponent({
   value,
   placeholder,
   onChange,
-  button = undefined,
 }: props) {
   return (
     <div className={"flex gap-1 items-center"}>
@@ -30,16 +24,6 @@ export default function InputComponent({
         placeholder={placeholder}
         onChange={onChange}
       />
-      {button && (
-        <ButtonComponent
-          onClick={button.event}
-          className={
-            "py-1 px-4 text-sm border border-[#77A4E8] text-[#FFFFFF] bg-[#77A4E8] rounded w-fit min-w-[75px] whitespace-nowrap h-full"
-          }
-        >
-          {button.children}
-        </ButtonComponent>
-      )}
     </div>
   );
 }
