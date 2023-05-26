c8f7463acdb7e36d914a1f54e9fcf4795ff7b5c7
이진희 <49019236+bebusl@users.noreply.github.com>
2023-05-27 06:58:49 +0900

    Refactor: mock 데이터 생성 util 함수이름 변경

--- src/mocks/utils/generateMockData.ts
+++ src/mocks/utils/generateMockData.ts
@@ -1,12 +1,12 @@
-import { getRandomInt } from '../../utils/random';
+import { generateRandomInt } from '../../utils/random';
 
 export function generateMockMember(uniqueKey: number) {
   return {
     memberId: 17 + uniqueKey,
     memberName: `신고한 사람${uniqueKey + 1}`,
-    memberCommentBadge: getRandomInt(5),
-    memberPostBadge: getRandomInt(5),
-    memberLikeBadge: getRandomInt(5),
+    memberCommentBadge: generateRandomInt(5),
+    memberPostBadge: generateRandomInt(5),
+    memberLikeBadge: generateRandomInt(5),
     memberProfileUrl: 'https://picsum.photos/200',
   };
 }
