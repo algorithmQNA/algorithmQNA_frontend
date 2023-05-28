6d170167a7242d73480059445ee146a310c6067b
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2023-05-28 22:01:36 +0900

    Feat: 글 쓰기 페이지 수정
    타이틀 입력란 컴포넌트화
    셀렉트 박스 로드 이벤트 추가 (수정 페이지)

---
+++ src/components/Board/PostWrite/TitleBlock.tsx
@@ -0,0 +1,21 @@
+import InputText from "../../Input/InputText";
+import React, {ChangeEvent} from "react";
+import {useRecoilState} from "recoil";
+import {PostWriteState} from "../../../storage/PostWrite/PostWrite";
+
+export default function PostWriteTitleBlock(){
+    const [state, setState] = useRecoilState(PostWriteState);
+    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setState((prev) => ({
+            ...prev,
+            title: e.target.value,
+        }));
+    };
+    console.log(state)
+    return(
+        <div className={'title-block'}>
+            <p className={'name-tag'}>제목</p>
+            <InputText defaultValue={state.title} onChange={changeTitle} />
+        </div>
+    )
+}
