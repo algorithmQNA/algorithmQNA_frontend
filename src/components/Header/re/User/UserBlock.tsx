47d5964edee6af53dc2c076fa9bd0a396f932ba7
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2023-05-28 15:50:30 +0900

    Refactor: 알람 데이터 수정
     api명세 완성 시 변경

--- src/components/Header/re/User/UserBlock.tsx
+++ src/components/Header/re/User/UserBlock.tsx
@@ -1,6 +1,30 @@
 import {useNavigate} from "react-router-dom";
 import {ChangeEvent, useState} from "react";
 import {FiBell} from "react-icons/fi";
+import {AlarmType} from "../../../../types/Alarm";
+
+const data:AlarmType[] = [
+    {
+        "alarmId": 2097,
+        "subjectMemberName": "testMember5",
+        "eventURL": "/post/4",
+        "checked": false,
+        "alarmType": "COMMENT_LIKE",
+        "commentId": 828,
+        "msg": "testMember5님이 당신의 댓글에 좋아요를 남겼습니다.",
+        "createdAt": "2023-05-26T17:11:17.506706"
+    },
+    {
+        "alarmId": 2095,
+        "subjectMemberName": "testMember5",
+        "eventURL": "/post/4",
+        "checked": false,
+        "alarmType": "COMMENT_LIKE",
+        "commentId": 827,
+        "msg": "testMember5님이 당신의 댓글에 좋아요를 남겼습니다.",
+        "createdAt": "2023-05-26T17:11:17.505708"
+    },
+]
 
 export default function HeaderUserBlock(){
     const navigate = useNavigate();
@@ -10,7 +34,7 @@ export default function HeaderUserBlock(){
     const setDisplayAlarm = (e: ChangeEvent<HTMLInputElement>) => {
         setState((prev) => ({
             ...prev,
-            alarm: e.target.checked,
+            alarm: e.target.checked
         }));
     };
     const post = (post_id: number) => {
@@ -27,6 +51,12 @@ export default function HeaderUserBlock(){
             },
         });
     };
+
+    const target = () =>{
+
+    }
+
+    const result:AlarmType[] = data
     return (
         <div
             className={
@@ -46,22 +76,19 @@ export default function HeaderUserBlock(){
                 {state.alarm && (
                     <ul
                         className={
-                            'absolute border border-primary min-w-[150px] -left-[60px] top-[150%] bg-white grid rounded shadow'
+                            'absolute border border-primary w-[200px] -left-[90px] top-[150%] bg-white grid rounded shadow'
                         }
                     >
-                        <li
-                            className={'text-content hover:text-primary p-2'}
-                            onClick={() => post(1)}
-                        >
-                            게시물
-                        </li>
-                        <li
-                            className={'text-content hover:text-primary p-2'}
-                            onClick={() => comment(1, 2)}
-                        >
-                            댓글
-
-                        </li>
+                        {
+                            result.map((li)=>(
+                                <li
+                                    className={'text-content hover:text-primary p-2 text-sm'}
+                                    onClick={() => post(1)}
+                                >
+                                    {li.msg}
+                                </li>
+                            ))
+                        }
                     </ul>
                 )}
             </label>
