6733192bd397cece2003026c97892ae803baca5a
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2023-05-28 23:23:36 +0900

    Feat: 셀렉트 박스 기능 수정
    1. 옵션의 하단 선이 마지막 옵션 값까지 적용 되서 겹쳐보이는 현상 수정
    2. selected props를 통해 기본 값 설정

--- src/components/Board/PostWrite/SelectBlock.tsx
+++ src/components/Board/PostWrite/SelectBlock.tsx
@@ -1,10 +1,10 @@
 import { SelectBox, SelectOption } from '../../DropDown/SelectBox';
 import React from 'react';
-import { useSetRecoilState } from 'recoil';
+import {useRecoilState} from 'recoil';
 import { PostWriteState } from '../../../storage/PostWrite/PostWrite';
 
 export default function PostWriteSelectBlock() {
-  const setState = useSetRecoilState(PostWriteState);
+  const [state,setState] = useRecoilState(PostWriteState);
   const selectBoard = (value: string) => {
     setState((prev) => ({
       ...prev,
@@ -21,14 +21,16 @@ export default function PostWriteSelectBlock() {
     <div className={'select-block'}>
       <div className={'board-select-block'}>
         <p className={'name-tag'}>게시판</p>
-        <SelectBox defaultText={'게시판 선택'} event={selectBoard}>
+        <SelectBox event={selectBoard} selected={String(state.postType)}>
+          <SelectOption value={''}>게시판 선택</SelectOption>
           <SelectOption value={'1'}>Q&A</SelectOption>
           <SelectOption value={'2'}>꿀팁</SelectOption>
         </SelectBox>
       </div>
       <div className={'category-select-block'}>
         <p className={'name-tag'}>카테고리</p>
-        <SelectBox defaultText={'분류 선택'} event={selectCategory}>
+        <SelectBox event={selectCategory} selected={String(state.postCategory)}>
+          <SelectOption value={''}>카테고리 선택</SelectOption>
           <SelectOption value={'1'}>카테고리1</SelectOption>
           <SelectOption value={'2'}>카테고리2</SelectOption>
         </SelectBox>
