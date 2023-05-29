5a410f85462fc8c41a63eb8f0515cae184dd5111
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2023-05-30 00:29:55 +0900

    Feat: 알람 스크롤 기능 구현
    맨 위로 이동했을 때와 맨 아래로 이동 했을 때 이벤트 구현

--- src/components/Header/re/User/UserBlock.tsx
+++ src/components/Header/re/User/UserBlock.tsx
@@ -1,5 +1,5 @@
 import {useNavigate} from "react-router-dom";
-import {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useRef, useState} from "react";
 import {FiBell} from "react-icons/fi";
 import {AlarmType} from "../../../../types/Alarm";
 
@@ -15,7 +15,7 @@ const data:AlarmType[] = [
         "createdAt": "2023-05-26T17:11:17.506706"
     },
     {
-        "alarmId": 2095,
+        "alarmId": 2098,
         "subjectMemberName": "testMember5",
         "eventURL": "/post/4",
         "checked": false,
@@ -24,12 +24,42 @@ const data:AlarmType[] = [
         "msg": "testMember5님이 당신의 댓글에 좋아요를 남겼습니다.",
         "createdAt": "2023-05-26T17:11:17.505708"
     },
+    {
+        "alarmId": 2099,
+        "subjectMemberName": "testMember5",
+        "eventURL": "/post/4",
+        "checked": false,
+        "alarmType": "COMMENT_LIKE",
+        "commentId": 828,
+        "msg": "testMember5님이 당신의 댓글에 좋아요를 남겼습니다.",
+        "createdAt": "2023-05-26T17:11:17.506706"
+    },
+    {
+        "alarmId": 2100,
+        "subjectMemberName": "testMember5",
+        "eventURL": "/post/4",
+        "checked": false,
+        "alarmType": "COMMENT_LIKE",
+        "commentId": 827,
+        "msg": "testMember5님이 당신의 댓글에 좋아요를 남겼습니다.",
+        "createdAt": "2023-05-26T17:11:17.505708"
+    },
+    {
+        "alarmId": 2101,
+        "subjectMemberName": "testMember5",
+        "eventURL": "/post/4",
+        "checked": false,
+        "alarmType": "COMMENT_LIKE",
+        "commentId": 828,
+        "msg": "testMember5님이 당신의 댓글에 좋아요를 남겼습니다.",
+        "createdAt": "2023-05-26T17:11:17.506706"
+    },
 ]
 
 export default function HeaderUserBlock(){
     const navigate = useNavigate();
     const [state, setState] = useState({
-        alarm: false,
+        alarm: false
     });
     const setDisplayAlarm = (e: ChangeEvent<HTMLInputElement>) => {
         setState((prev) => ({
@@ -55,8 +85,34 @@ export default function HeaderUserBlock(){
     const target = () =>{
 
     }
+    const [result,setResult] = useState<AlarmType[]>(data)
+    const button = useRef<HTMLButtonElement>(null)
+    const childHeight = 50
+    const getNewData = (e:React.UIEvent<HTMLUListElement>) =>{
+        console.log(e.currentTarget.clientHeight)
+        if(!button.current) return
+        if(e.currentTarget.scrollTop === 0){
+            console.log('스크롤이 맨 위로 이동')
+        }
+        else if(e.currentTarget.scrollTop + (e.currentTarget.clientHeight-childHeight) >= button.current?.offsetTop){
+            console.log('스크롤이 맨 아래로 이동')
+            setResult([...result,{
+                "alarmId": result[result.length-1].alarmId+1,
+                "subjectMemberName": "testMember5",
+                "eventURL": "/post/4",
+                "checked": false,
+                "alarmType": "COMMENT_LIKE",
+                "commentId": 828,
+                "msg": "testMember5님이 당신의 댓글에 좋아요를 남겼습니다.",
+                "createdAt": "2023-05-26T17:11:17.506706"
+            }])
+        }
+    }
 
-    const result:AlarmType[] = data
+
+
+
+
     return (
         <div
             className={
@@ -76,19 +132,22 @@ export default function HeaderUserBlock(){
                 {state.alarm && (
                     <ul
                         className={
-                            'absolute border border-primary w-[200px] -left-[90px] top-[150%] bg-white grid rounded shadow'
+                            'alarm absolute border border-primary w-[200px] -left-[550%] top-[175%] bg-white  rounded shadow px-2 overflow-auto grid max-h-[250px] box-content'
                         }
+                        onScroll={getNewData}
                     >
                         {
                             result.map((li)=>(
                                 <li
-                                    className={'text-content hover:text-primary p-2 text-sm'}
+                                    key={li.alarmId}
+                                    className={`text-content hover:text-primary text-sm h-[${childHeight}px] flex items-center`}
                                     onClick={() => post(1)}
                                 >
-                                    {li.msg}
+                                    <span>{li.msg}</span>
                                 </li>
                             ))
                         }
+                        <button className={`h-[${childHeight}px] flex items-center text-sm text-primary`} ref={button}><span>더보기</span></button>
                     </ul>
                 )}
             </label>
