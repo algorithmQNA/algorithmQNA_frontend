bcdbb3b15814ec80aa93753cada26239738b08ad
이진희 <49019236+bebusl@users.noreply.github.com>
2023-05-31 14:28:42 +0900

    Fix: Post DTO 타입 수정

--- src/types/Post/Post.ts
+++ src/types/Post/Post.ts
@@ -1,75 +1,76 @@
-export interface PostRow{
-    title:string
-    memberId:number
-    memberName:string
-    memberProfileUrl:string
-    memberRole:string
-    createdAt:string
-    viewCount:number
-    commentCount:number
+export interface PostRow {
+  title: string;
+  memberId: number;
+  memberName: string;
+  memberProfileUrl: string;
+  memberRole: string;
+  createdAt: string;
+  viewCount: number;
+  commentCount: number;
 }
-export interface PostList{
-    posts:PostRow[]
-    currentPage:number
-    totalPageCount:number
-    next:boolean
-    prev:boolean
-    size:number
+export interface PostList {
+  posts: PostRow[];
+  currentPage: number;
+  totalPageCount: number;
+  next: boolean;
+  prev: boolean;
+  size: number;
 }
-export interface PostViewMember{
-    "memberId": number,
-    "memberName": string,
-    "memberRole": string,
-    "memberCommentBadge": number,
-    "memberPostBadge": number,
-    "memberLikeBadge": number,
-    "memberProfileUrl": string
+export interface PostViewMember {
+  memberId: number;
+  memberName: string;
+  memberRole: string;
+  memberCommentBadge: number;
+  memberPostBadge: number;
+  memberLikeBadge: number;
+  memberProfileUrl: string;
 }
-export interface PostViewChildComment{
-    "commentId": number,
-    "memberId": number,
-    "memberName": string,
-    "content": string,
-    "createdAt": string,
-    "updatedAt": string,
-    "likeCnt": number,
-    "dislikeCnt": number,
-    "depth" : number,
-    "hasChild" : boolean
+export interface PostViewChildComment {
+  commentId: number;
+  memberId: number;
+  memberName: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  likeCnt: number;
+  dislikeCnt: number;
+  depth: number;
+  hasChild: boolean;
 }
-export interface PostViewComment{
-    "commentId": number,
-    "member": PostViewMember,
-    "content": string,
-    "createdAt": string,
-    "updatedAt": string,
-    "likeCnt": number,
-    "dislikeCnt": number,
-    "depth" : number,
-    "hasChild" : boolean,
-    "childCommentList":PostViewChildComment[]
-    "childSize":number
+export interface PostViewComment {
+  commentId: number;
+  member: PostViewMember;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  likeCnt: number;
+  dislikeCnt: number;
+  depth: number;
+  hasChild: boolean;
+  childCommentList: PostViewChildComment[];
+  childSize: number;
 }
-export interface PostView{
-    "postId": number
-    "postTitle":string,
-    "postContent": string
-    "createdAt": string
-    "postLikeCnt": number,
-    "postDislikeCnt": number,
-    "isLiked": boolean,
-    "totalCommentCnt": number
-    "totalPageSize": number,
-    "member":PostViewMember
-    "commentList":PostViewComment[]
-    "page": number,
-    "next": boolean,
-    "prev": boolean,
-    "size": number
+export interface PostView {
+  postId: number;
+  postTitle: string;
+  postContent: string;
+  createdAt: string;
+  postLikeCnt: number;
+  postDislikeCnt: number;
+  isLiked: null | boolean;
+  isPinned: boolean;
+  totalCommentCnt: number;
+  totalPageSize: number;
+  member: PostViewMember;
+  commentList: PostViewComment[];
+  page: number;
+  next: boolean;
+  prev: boolean;
+  size: number;
 }
-export interface PostWrite{
-    title:string
-    content:string
-    kind:null | number
-    category:null | number
+export interface PostWrite {
+  title: string;
+  content: string;
+  kind: null | number;
+  category: null | number;
 }
