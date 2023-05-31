46d8eb5fd2a916e2982105e2ff95168f3a85aae0
이진희 <49019236+bebusl@users.noreply.github.com>
2023-05-31 18:46:00 +0900

    Feat: 공지사항 필터링 적용

--- src/pages/Admin/Notice.tsx
+++ src/pages/Admin/Notice.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import AdminTableRow from '../../components/TableRow/AdminTableRow';
 
 import IconButton from '../../components/Button/IconButton';
@@ -7,51 +7,74 @@ import { useQuery } from 'react-query';
 import { getNotificationList } from '../../apis/adminApi';
 import { Suspense } from 'react';
 import AdminTableRowSkeleton from '../../components/TableRow/AdminTableRowSkeleton';
+import { SelectBox, SelectOption } from '../../components/DropDown/SelectBox';
+import { POST_CATEGORY } from '../../constants/PostCategory';
+import Pagination from '../../components/Pagination/Pagination';
 
 const TmpRow = () => {
+  /** url에서 쿼리파라미터를 가져와서 api 요청할 때 같이 보냄 */
   const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const page = queryParams.get('page') || 1;
+  const tags = queryParams.getAll('tags') || 1;
 
-  const page =
-    (new URLSearchParams(location.search).get('page') as unknown as number) ||
-    1;
-
-  const { data } = useQuery({
-    queryKey: ['notification', page],
-    queryFn: () => getNotificationList({ type: 'ALL', page }),
+  const notificationQuery = useQuery({
+    queryKey: ['notification', page, tags],
+    queryFn: () => getNotificationList({ type: 'ALL', page: +page }),
     suspense: true,
   });
-  return (
-    <div className="flex flex-col gap-3">
-      {data?.data.list.map((t: any) => (
-        <AdminTableRow title={t.title} key={t.id} id={t.id} date={t.date} />
-      ))}
-    </div>
-  );
+
+  const data = notificationQuery.data?.data;
+  if (!!data?.list.length)
+    return (
+      <div className="flex flex-col gap-3">
+        {data.list.map((t: any) => (
+          <AdminTableRow title={t.title} key={t.id} id={t.id} date={t.date} />
+        ))}
+        <Pagination listLength={10} postLength={20} />
+      </div>
+    );
+  //검색은 성공했으나 등록된 공지사항 개수가 0개일 떄
+  return <div>등록된 글이 없습니다</div>;
 };
 
 function Notice() {
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const handleCreateNoticeButtonClick = () => {
+    navigate('/post/write');
+  };
 
-  const onCreateNoticeButtonClickHandler = () => {
-    navigate('/write');
+  const handleSelectOptionChange = (tag: string) => {
+    setSearchParams((prev) => {
+      const tags = prev.getAll('tags');
+      const setOfTags = new Set(tags);
+      if (setOfTags.has(tag)) setOfTags.delete(tag);
+      else setOfTags.add(tag);
+
+      //for immutability
+      const newQuery = new URLSearchParams(searchParams);
+      newQuery.delete('tags');
+      setOfTags.forEach((tag) => newQuery.append('tags', tag));
+      return newQuery;
+    });
   };
 
   return (
-    <>
-      <div className="flex justify-between px-2 gap-2 border border-border">
-        <select name="전체" className="text-sm">
-          <option value="전체" className="text-sm">
-            전체
-          </option>
-          <option value="전체" className="text-sm">
-            Q&A
-          </option>
-          <option value="전체" className="text-sm">
-            꿀팁
-          </option>
-        </select>
+    <div>
+      <div className="flex justify-between p-2 gap-2 border border-border">
+        <div className="w-1/3">
+          <SelectBox defaultText="전체" event={handleSelectOptionChange}>
+            {Object.entries(POST_CATEGORY).map((t) => (
+              <SelectOption value={t[0]} key={t[0]}>
+                {t[1]}
+              </SelectOption>
+            ))}
+          </SelectBox>
+        </div>
+
         <IconButton
-          onClick={onCreateNoticeButtonClickHandler}
+          onClick={handleCreateNoticeButtonClick}
           Icon={<AiFillNotification style={{ display: 'inline' }} />}
         >
           공지사항 작성
@@ -59,18 +82,18 @@ function Notice() {
       </div>
       <Suspense
         fallback={
-          <>
+          <div className="flex flex-col gap-2">
             {Array(10)
               .fill(null)
               .map((_, idx) => (
                 <AdminTableRowSkeleton key={idx} />
               ))}
-          </>
+          </div>
         }
       >
         <TmpRow />
       </Suspense>
-    </>
+    </div>
   );
 }
 
