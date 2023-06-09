7c61512ef9385c1d22524e99ac0f6067f95e71bd
이진희 <49019236+bebusl@users.noreply.github.com>
2023-06-09 23:26:13 +0900

    Feat: cross-env 세팅 * 환경(윈도우/리눅스/맥 ...)에 상관없이 동일한 스크립트 실행하면 환경변수 세팅되도록 cross-env 사용

--- src/components/Board/PostWrite/WriteBtn.tsx
+++ src/components/Board/PostWrite/WriteBtn.tsx
@@ -4,15 +4,34 @@ import { useMutation } from 'react-query';
 import { createPostRequest } from '../../../apis/postApi';
 import { useRecoilValue } from 'recoil';
 import { PostWriteState } from '../../../storage/PostWrite/PostWrite';
-import {PostCategory, PostType, PostWrite} from '../../../types/Post/Post';
+import { PostCategory, PostType, PostWrite } from '../../../types/Post/Post';
 import { AxiosError } from 'axios';
 import { ErrorType } from '../../../types/Error';
 
 export default function PostWriteBtn() {
   const state = useRecoilValue(PostWriteState);
+  /**
+   * MESSAGE:
+   * 포스팅 테스트한다고 해당 부분 임시로 수정해두었습니다. 오승님이 편한방식대로 수정하시면 됩니다!
+   * author:이진희
+   */
   const { mutate } = useMutation(
-    ({ title, content, postCategory, postType, keyWord, imageIds }: PostWrite) =>
-      createPostRequest(title, content, postCategory, postType as PostType,keyWord,imageIds),
+    ({
+      title,
+      content,
+      postCategory,
+      postType,
+      keyWord,
+      imageIds,
+    }: PostWrite) =>
+      createPostRequest({
+        title,
+        content,
+        category: postCategory,
+        contentType: postType as PostType,
+        keyWords: keyWord,
+        imageIds,
+      }),
     {
       onSuccess: () => {
         alert('작성 완료 됐습니다. \n 게시판 목록으로 돌아갑니다.');
