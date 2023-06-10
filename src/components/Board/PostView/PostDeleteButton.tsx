c0159a7f32cce00fb9109f68bded64c2bbcfde32
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2023-06-10 16:45:17 +0900

    Feat: 게시물 삭제 버튼

---
+++ src/components/Board/PostView/PostDeleteButton.tsx
@@ -0,0 +1,37 @@
+import {useMutation} from "react-query";
+import axios, {AxiosError} from "axios";
+import {useLocation, useNavigate} from "react-router-dom";
+
+export default function PostDeleteButton(){
+    const nav = useNavigate()
+    const location = useLocation();
+    const params = new URLSearchParams(location.search).get('pid');
+    const pid = parseInt(params as string);
+
+    const {mutate} = useMutation(({id}:{id:number})=>axios.delete(`/post/${id}`),{
+        onError:(error:AxiosError)=>{
+            if(error.status === 401){
+                alert('게시물을 삭제할 권한이 없습니다')
+                nav(-1)
+            }
+            else if(error.status === 404){
+                alert('게시물이 존재하지 않습니다')
+                nav(-1)
+            }
+        },
+        onSuccess:()=>{
+            alert('성공적으로 게시물을 삭제했습니다.')
+            nav(-1)
+        }
+    })
+
+    const deletePost = (id:number) =>{
+        mutate({id})
+    }
+
+    return(
+        <button onClick={()=>deletePost(pid)}>
+            삭제
+        </button>
+    )
+}
