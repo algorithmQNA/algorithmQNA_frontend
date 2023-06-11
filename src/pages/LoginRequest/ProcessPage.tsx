e70e6c101a2497b6266e5948e2a38ee6088c007f
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2023-06-11 23:13:15 +0900

    Feat: 구글 로그인 진행 중 아이콘 추가

--- src/pages/LoginRequest/ProcessPage.tsx
+++ src/pages/LoginRequest/ProcessPage.tsx
@@ -6,5 +6,9 @@ export default function LoginProcessPage() {
   const code = new URLSearchParams(search).get('code');
   const state = new URLSearchParams(search).get('state');
   useSetLogin({ code, state });
-  return <div></div>;
+  return (
+      <div className={'fixed z-[1000] top-0 left-0 w-full h-screen bg-white flex justify-center items-center select-none'}>
+        <img src={'/svg/spinner.svg'}/>
+      </div>
+  )
 }
