834d66580e123f4e7afce0504e72b4e1d4d9f602
이진희 <49019236+bebusl@users.noreply.github.com>
2023-06-13 23:40:08 +0900

    Feat: 게시글 뷰 페이지에 댓글 컴포넌트 합침

--- src/pages/CommentTest.tsx
+++ src/pages/CommentTest.tsx
@@ -1,9 +1,9 @@
 import React, { MouseEventHandler, useState } from 'react';
-import CommentView from '../components/CommentView/Comment';
 import { useQuery } from 'react-query';
 import {
   Link,
   useLocation,
+  useNavigate,
   useParams,
   useSearchParams,
 } from 'react-router-dom';
@@ -12,7 +12,8 @@ import IconButton from '../components/Button/IconButton';
 import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 import { BsArrowReturnRight } from 'react-icons/bs';
 import ButtonComponent from '../components/Button/ButtonComponent';
-import PageTitle from '../components/PageTitle/PageTitle';
+import CommentWrite from '../components/CommentView/CommentWrite';
+import CommentView from '../components/CommentView/Comment';
 
 function CommentTest() {
   //const [selectedComment, setSelectedComment] = useState(-1);
@@ -20,6 +21,7 @@ function CommentTest() {
   const { postId } = useParams();
   const [searchParams, setSearchParams] = useSearchParams();
   const [isScrolling, setIsScrolling] = useState(false);
+  const navigate = useNavigate();
   const location = useLocation();
   const page = new URLSearchParams(location.search).get('page') || 1;
   const { data: res } = useQuery({
@@ -28,16 +30,20 @@ function CommentTest() {
       if (postId) return getPostRequest(+postId);
       return getPostRequest(2000);
     },
+    onError: (e) => {
+      alert('존재하지 않는 게시글입니다.');
+      navigate(-1);
+    },
   });
 
-  const data = res?.data;
+  const data = res?.data.data;
 
   const marginLeft: { [key: string]: string } = {
     0: 'ml-0',
     1: 'ml-6',
     2: 'ml-12',
   };
-
+  console.log(data);
   if (data?.commentList) {
     // TODO :: 브랜치 합치고 주석 풀어주기
     // const selectedComment =
@@ -72,31 +78,31 @@ function CommentTest() {
     );
 
     return (
-      <>
-        <PageTitle>댓글테스트</PageTitle>
-        <div className="main-content">
-          <Controller />
-          {/*<CommentWrite />*/}
-          <div className="w-full flex flex-col">
-            {data.commentList.map((t) => (
-              <div
-                className={`${marginLeft[t.depth]} flex flex-row my-4 gap-2 ${
-                  isScrolling &&
-                  t.commentId === selectedComment &&
-                  'bg-yellow-50 animate-pulse'
-                }`}
-                id={`${t.commentId}`}
-                key={`${t.commentId}`}
-                tabIndex={-1}
-              >
-                {!!t.depth && <BsArrowReturnRight className="text-gray-500" />}
-                {/*<CommentView {...t} key={t.commentId} />*/}
-              </div>
-            ))}
-          </div>
-          <Controller />
+      <div>
+        <CommentWrite />
+        <div className="w-full flex flex-col">
+          {data.commentList.map((comment) => (
+            <div
+              className={`${
+                marginLeft[comment.depth]
+              } flex flex-row my-4 gap-2 ${
+                isScrolling &&
+                comment.commentId === selectedComment &&
+                'bg-yellow-50 animate-pulse'
+              }`}
+              id={`${comment.commentId}`}
+              key={`${comment.commentId}`}
+              tabIndex={-1}
+            >
+              {!!comment.depth && (
+                <BsArrowReturnRight className="text-gray-500" />
+              )}
+              <CommentView {...comment} key={comment.commentId} />
+            </div>
+          ))}
         </div>
-      </>
+        <Controller />
+      </div>
     );
   }
   return <div>잘못된 페이지입니다</div>;
