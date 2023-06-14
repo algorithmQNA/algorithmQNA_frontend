9db177fc25d282054eafc4733750523c0d8c9cdb
이진희 <49019236+bebusl@users.noreply.github.com>
2023-06-14 23:48:28 +0900

    Refactor: 쿼리 파라미터 변경 Link -> useSearchParams 사용

--- src/components/CommentView/Controller.tsx
+++ src/components/CommentView/Controller.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { getPostRequest } from '../../apis/postApi';
 import IconButton from '../Button/IconButton';
 import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
-import { useQuery } from 'react-query';
 import ButtonComponent from '../Button/ButtonComponent';
-import { getPostRequest } from '../../apis/postApi';
 
 function Controller() {
-  const location = useLocation();
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const page = searchParams.get('page') || 0;
   const get = useQuery('post-view', () => getPostRequest(+page));
@@ -21,17 +20,29 @@ function Controller() {
   const prevPage = +page <= MIN_PAGE ? MIN_PAGE : +page - 1;
   const nextPage = +page >= MAX_PAGE ? MAX_PAGE : +page + 1;
 
+  /** 다른 query string은 수정하지 않고 page query string만 수정*/
+  const updatePageParameter = (page: number) => {
+    setSearchParams((old) => {
+      const newParams = new URLSearchParams(old);
+      newParams.set('page', `${page}`);
+      return newParams;
+    });
+  };
+
+  const handlePrevBtnClick = () => {
+    if (prevPage !== page) updatePageParameter(prevPage);
+  };
+  const handleNextBtnClick = () => {
+    if (nextPage !== page) updatePageParameter(nextPage);
+  };
+
   return (
     <div className="grow-0 m-4 w-fit float-right flex flex-row items-center sticky bottom-5 right-5 bg-box-bg border border-border rounded-full p-4 shadow-sm z-50">
-      <Link to={`${location.pathname}?page=${prevPage}`}>
-        <IconButton Icon={<BiChevronLeft />}></IconButton>
-      </Link>
+      <IconButton Icon={<BiChevronLeft />} onClick={handlePrevBtnClick} />
       <div>
         {+page + 1} / {MAX_PAGE + 1}
       </div>
-      <Link to={`${location.pathname}?page=${nextPage}`}>
-        <IconButton Icon={<BiChevronRight />}></IconButton>
-      </Link>
+      <IconButton Icon={<BiChevronRight />} onClick={handleNextBtnClick} />
       <ButtonComponent onClick={handleGoToPinnedBtnClick}>
         채택된 답변보기
       </ButtonComponent>
