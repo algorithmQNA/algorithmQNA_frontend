7f997a79b844beb232b4c20de5b3aa580ed6f90f
이진희 <49019236+bebusl@users.noreply.github.com>
2023-06-18 02:31:00 +0900

    Fix: 댓글 컨트롤러 페이지네이션 잘못 계산되는 현상 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,36 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
-import {AxiosError} from "axios";
 
-// if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_MOCK) {
-//   const { worker } = require('./mocks/browser');
-//   worker.start({ quiet: true, onUnhandledRequest: 'bypass' });
-// }
+if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_MOCK) {
+  const { worker } = require('./mocks/browser');
+  worker.start({ onUnhandledRequest: 'bypass' });
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-        onError:(err)=>{
-            console.log(err)
-        },
-        staleTime: 5 * 60 * 1000,
-        retry: false,
+      onError: (err) => {
+        console.log(err);
+      },
+      staleTime: 5 * 60 * 1000,
+      retry: false,
     },
   },
 });
 
 root.render(
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <App />
-        </RecoilRoot>
-      </QueryClientProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  </BrowserRouter>
 );
