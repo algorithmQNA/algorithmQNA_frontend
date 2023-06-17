5cf9b8521c74f7ae7779737976841196c8b63e60
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2023-06-17 20:35:17 +0900

    Feat: 헤더 로고 설정
    로고 설정 및 불필요 폴더 제거

--- src/components/Header/re/Link/LinkBlock.tsx
+++ src/components/Header/re/Link/LinkBlock.tsx
@@ -1,14 +1,17 @@
 import {Link} from "react-router-dom";
+import LogoIcon from "../../../Icon/Icon";
 
 export default function HeaderLinkBlock(){
     return(
         <nav
             className={
-                'flex justify-between text-base md:text-xl font-bold gap-2 md:gap-6 whitespace-nowrap col-span-1 w-fit'
+                'flex justify-between text-base md:text-xl items-center font-bold gap-2 md:gap-6 whitespace-nowrap col-span-1 w-fit'
             }
         >
             <Link to={'/'} className={'mr-2 md:mr-6'}>
-                Logo
+                <span className={'block w-[75px] h-auto text-white hover:text-primary transition-colors duration-300'}>
+                    <LogoIcon/>
+                </span>
             </Link>
             <Link className={'hover:text-primary transition-colors duration-300'} to={'/board/q&a'}>Q&A</Link>
             <Link className={'hover:text-primary transition-colors duration-300'} to={'/board/tip'}>꿀팁</Link>
