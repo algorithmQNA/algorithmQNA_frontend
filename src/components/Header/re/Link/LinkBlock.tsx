a6989d13730e30b6ba6f322ed301c4df98df3cda
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2023-06-17 22:04:55 +0900

    Design: 헤더 링크 활성화
    해당하는 페이지에 있으면 활성화 표시

--- src/components/Header/re/Link/LinkBlock.tsx
+++ src/components/Header/re/Link/LinkBlock.tsx
@@ -1,7 +1,8 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import LogoIcon from "../../../Icon/Icon";
 
 export default function HeaderLinkBlock(){
+    const {pathname} = useLocation()
     return(
         <nav
             className={
@@ -13,8 +14,8 @@ export default function HeaderLinkBlock(){
                     <LogoIcon/>
                 </span>
             </Link>
-            <Link className={'hover:text-primary transition-colors duration-300'} to={'/board/q&a'}>Q&A</Link>
-            <Link className={'hover:text-primary transition-colors duration-300'} to={'/board/tip'}>꿀팁</Link>
+            <Link className={`hover:text-primary transition-colors duration-300 ${pathname === '/board/q&a' && 'text-primary'}`} to={'/board/q&a'}>Q&A</Link>
+            <Link className={`hover:text-primary transition-colors duration-300 ${pathname === '/board/tip' && 'text-primary'}`} to={'/board/tip'}>꿀팁</Link>
         </nav>
     )
 }
