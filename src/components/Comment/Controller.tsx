528e02adeca64312e540a506dcd07e832bdc8f7e
이진희 <49019236+bebusl@users.noreply.github.com>
2023-06-18 02:31:00 +0900

    Fix: 댓글 컨트롤러 페이지네이션 잘못 계산되는 현상 수정

--- src/components/Comment/Controller.tsx
+++ src/components/Comment/Controller.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useSearchParams } from 'react-router-dom';
+import {
+  useLocation,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { getPostRequest } from '../../apis/postApi';
 import IconButton from '../Button/IconButton';
@@ -8,17 +13,33 @@ import ButtonComponent from '../Button/ButtonComponent';
 
 function Controller() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  const curLocation = location.pathname + location.search;
+  const { pid = -1 } = useParams();
+  const navigate = useNavigate();
 
   const page = searchParams.get('page') || 0;
-  const get = useQuery('post-view', () => getPostRequest(+page));
+  const get = useQuery(['post-view', +pid], () => getPostRequest(+page));
 
-  const handleGoToPinnedBtnClick = () => {};
-
+  const handleGoToPinnedBtnClick = () => {
+    const PIN_COMMENT_ID = get.data?.data.data.pinnedComment?.commentId;
+    console.log(get.data);
+    if (PIN_COMMENT_ID)
+      navigate(curLocation, {
+        state: {
+          highlighting: true,
+          mode: 'pin',
+          commentId: +PIN_COMMENT_ID,
+        },
+      });
+    else alert('채택된 댓글이 없습니다');
+  };
+  console.log(get.data?.data.data);
   const MIN_PAGE = 0;
   const MAX_PAGE = get.data?.data.data.totalPageSize || 0;
 
   const prevPage = +page <= MIN_PAGE ? MIN_PAGE : +page - 1;
-  const nextPage = +page >= MAX_PAGE ? MAX_PAGE : +page + 1;
+  const nextPage = +page > MAX_PAGE - 1 ? MAX_PAGE - 1 : +page + 1;
 
   /** 다른 query string은 수정하지 않고 page query string만 수정*/
   const updatePageParameter = (page: number) => {
@@ -30,17 +51,17 @@ function Controller() {
   };
 
   const handlePrevBtnClick = () => {
-    if (prevPage !== page) updatePageParameter(prevPage);
+    if (prevPage > MIN_PAGE) updatePageParameter(prevPage);
   };
   const handleNextBtnClick = () => {
-    if (nextPage !== page) updatePageParameter(nextPage);
+    if (nextPage < MAX_PAGE) updatePageParameter(nextPage);
   };
 
   return (
     <div className="grow-0 m-4 w-fit float-right flex flex-row items-center sticky bottom-5 right-5 bg-box-bg border border-border rounded-full p-4 shadow-sm z-50">
       <IconButton Icon={<BiChevronLeft />} onClick={handlePrevBtnClick} />
       <div>
-        {+page + 1} / {MAX_PAGE + 1}
+        {+page + 1} / {MAX_PAGE}
       </div>
       <IconButton Icon={<BiChevronRight />} onClick={handleNextBtnClick} />
       <ButtonComponent onClick={handleGoToPinnedBtnClick}>
