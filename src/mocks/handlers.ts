deae1ae8b9618faa5facf54cd763bd3cf401b9b0
이진희 <49019236+bebusl@users.noreply.github.com>
2023-06-17 19:36:39 +0900

    Fix: 목서버 타입에러나서 빌드 실패하는 것 수정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -13,7 +13,19 @@ const handlers = [
       );
       // ...
     }
-  )
+  ),
+  rest.get(
+    process.env.REACT_APP_API_BASE_URL + '/alarm',
+    async (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          message: 'success',
+        })
+      );
+      // ...
+    }
+  ),
 ];
 
 export default handlers;
