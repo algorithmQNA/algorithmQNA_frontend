1a10f8de4bbb02dcdf1d8a23bc21b97e260985a4
이진희 <49019236+bebusl@users.noreply.github.com>
2023-06-20 10:29:04 +0900

    Refactor: 댓글 신고내역 컴포넌트 분리
    * 게시글 신고내역과 거의 동일함

--- src/components/Report/Comment/ReportComment.tsx
+++ src/components/Report/Comment/ReportComment.tsx
@@ -6,10 +6,14 @@ import { useSearchParams } from 'react-router-dom';
 
 import ReportCommentTableRow from './ReportCommentTableRow';
 import MessageBox from '../../MessageBox';
+import ReportCommentModal, {
+  useReportCommentModal,
+} from './ReportCommentModal';
 
 function ReportComment() {
   const [searchParams] = useSearchParams();
   const page = searchParams.get('page') || 0;
+  const { setModalContent } = useReportCommentModal();
 
   const { data } = useQuery({
     queryKey: ['reportedComment', +page],
@@ -25,25 +29,49 @@ function ReportComment() {
 
   if (isEmpty) return <MessageBox msg={`😊 신고내역이 없습니다!`} />;
   return (
-    <div className="flex flex-col gap-2 ">
-      {reportComments?.map((comment, idx) => {
-        const { member, createdAt, postId, content } = comment;
-        return (
-          <ReportCommentTableRow
-            id={postId}
-            date={createdAt}
-            title={`${postId}에 달린 댓글`}
-            member={member}
-            content={content}
-            key={`comment${idx}`}
-          />
-        );
-      })}
-      <Pagination
-        pageCount={data?.data.data.totalPageSize || 0}
-        listLength={data?.data.data.size || 0}
-      />
-    </div>
+    <>
+      <ReportCommentModal />
+      <div className="flex flex-col gap-2 ">
+        {reportComments?.map((comment, idx) => {
+          const {
+            member,
+            createdAt,
+            postId,
+            content,
+
+            dislikeCnt,
+            likeCnt,
+          } = comment;
+          return (
+            <div
+              className="cursor-pointer"
+              onClick={() =>
+                setModalContent({
+                  contentQueryKey: ['reportedComment', +page],
+                  reportListQueryKey: ['reportedCommentList', postId],
+                  idx: idx,
+                  open: true,
+                })
+              }
+            >
+              <ReportCommentTableRow
+                id={postId}
+                date={createdAt}
+                title={`${postId}에 달린 댓글`}
+                member={member}
+                dislikeCnt={dislikeCnt}
+                likeCnt={likeCnt}
+                key={`comment${idx}`}
+              />
+            </div>
+          );
+        })}
+        <Pagination
+          pageCount={data?.data.data.totalPageSize || 0}
+          listLength={data?.data.data.size || 0}
+        />
+      </div>
+    </>
   );
 }
 
--- src/components/Report/Comment/ReportCommentTableRow.tsx
+++ src/components/Report/Comment/ReportCommentTableRow.tsx
@@ -2,177 +2,81 @@ import { useState } from 'react';
 import useModal from '../../../hooks/useModal';
 import Modal from '../../Modal/Modal';
 
-import { setDateWritten } from '../../../utils/TextProcessing';
+import { setDateWritten, setOverValue } from '../../../utils/TextProcessing';
 import { BiTimeFive } from 'react-icons/bi';
 
-import ButtonComponent from '../../Button/ButtonComponent';
 import { MemberBrief } from '../../../types/member';
-import {
-  deleteReportedPostRequest,
-  getReportedCommentDetailRequest,
-} from '../../../apis/adminApi';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
-import UserProfile from '../../UserProfile/UserProfile';
-import IconButton from '../../Button/IconButton';
-import { AiOutlineClose } from 'react-icons/ai';
-import ReportTag from '../ReportTag';
+import { FiMessageSquare, FiThumbsDown, FiThumbsUp } from 'react-icons/fi';
 
 interface AdminPageTableRowProps {
+  id: number;
   title?: string;
   date: string;
-  id: number;
-  content: string;
   member: MemberBrief;
+  dislikeCnt: number;
+  likeCnt: number;
 }
 export default function ReportCommentTableRow({
+  id,
   title,
   date,
-  id,
-  content,
+  member,
+  dislikeCnt,
+  likeCnt,
 }: AdminPageTableRowProps) {
-  const reportManageModal = useModal();
-  const deletePostModal = useModal();
-  const page = 0;
-
-  const queryClient = useQueryClient();
-
-  //TODO: useInfiniteQuery로 변경
-  const { data } = useQuery(['reportCommentList', page], () =>
-    getReportedCommentDetailRequest(id, page)
+  return (
+    // <div className={`p-4 gap-1.5 bg-box-bg border border-border relative`}>
+    //   <div className={'flex flex-row justify-between h-full w-full'}>
+    //     <div>
+    //       <span className={'font-bold text-sm truncate max-w-[200px]'}>
+    //         {title}
+    //       </span>
+    //       <div className={'flex items-center justify-between w-full text-xs'}>
+    //         <div>
+    //           <BiTimeFive style={{ display: 'inline' }} />
+    //           <span className={'text-[#9ca3af] text-xs text-right'}>
+    //             {setDateWritten(date)}
+    //           </span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     {/* <div className="text-right absolute bottom-4 right-4">
+    //           <ButtonComponent onClick={handleClickViewButton}>
+    //             신고 사유 보기
+    //           </ButtonComponent>
+    //         </div> */}
+    //   </div>
+    // </div>
+    <div className={`p-4 bg-box-bg border border-border`}>
+      <div>
+        <span className="text-sm text-gray-600">{member.memberName}</span>
+        <span className={'text-xs text-gray-400'}>@{member.memberId}</span>
+        <p className={'font-bold truncate max-w-[200px]'}>{title}</p>
+      </div>
+      <div
+        className={
+          'flex items-center content-end justify-end w-full text-xs gap-1'
+        }
+      >
+        <span>
+          <BiTimeFive size={14} />
+        </span>
+        <span className={'text-[#9ca3af] text-xs text-right'}>
+          {setDateWritten(date)}
+        </span>
+        <span>
+          <FiThumbsUp size={14} />
+        </span>
+        <span className={'text-[#9ca3af] text-xs text-right'}>
+          {setOverValue(likeCnt, 99)}
+        </span>
+        <span>
+          <FiThumbsDown size={14} />
+        </span>
+        <span className={'text-[#9ca3af] text-xs text-right'}>
+          {setOverValue(dislikeCnt, 99)}
+        </span>
+      </div>
+    </div>
   );
-  const { mutate: deleteReportedMutate } = useMutation(
-    deleteReportedPostRequest,
-    {
-      onSettled: () => {
-        deletePostModal.closeModal();
-        reportManageModal.closeModal();
-        queryClient.invalidateQueries(['reportedComment']);
-      },
-    }
-  );
-
-  const [checkedList, setCheckedList] = useState<Set<number>>(new Set());
-
-  /** 삭제 Confirm 모달 관련 함수들 */
-  const handleClickViewButton = () => {
-    reportManageModal.openModal();
-  };
-
-  /** 신고당한 게시글 삭제 요청 */
-  const handleRemoveBtnClick = () => {
-    deletePostModal.openModal();
-  };
-  if (data)
-    return (
-      <>
-        {deletePostModal.open && (
-          <Modal
-            onClose={deletePostModal.closeModal}
-            onConfirm={() => {
-              deleteReportedMutate(id);
-            }}
-            onCancel={deletePostModal.closeModal}
-          >
-            <div>정말 게시글을 삭제하시겠습니까?</div>
-          </Modal>
-        )}
-        {reportManageModal.open && (
-          <Modal
-            title={`${title} 신고관리창`}
-            onClose={reportManageModal.closeModal}
-            size="lg"
-          >
-            <div className="flex justify-stretch w-full h-[32rem] overflow-auto">
-              <section className="flex-grow basis-1/2 h-full overflow-auto">
-                <div className="flex justify-between bg-box-bg p-2 border border-border">
-                  <UserProfile {...data.data.data.member} />
-                  <div className="flex flex-row items-end"></div>
-                </div>
-                <div
-                  className="ck ck-content p-2 text-left"
-                  dangerouslySetInnerHTML={{
-                    __html: content,
-                  }}
-                />
-              </section>
-              <section className="flex-grow basis-1/2 h-full overflow-auto">
-                <p className="font-semibold text-left">{title} 신고사유</p>
-                <div className="bg-box-bg">
-                  {data.data.data.commentReports.map((report, idx) => {
-                    return (
-                      <div className="w-full border text-left p-2">
-                        <div className="w-full flex flex-row justify-between">
-                          <div className="flex flex-row gap-2">
-                            <input
-                              type="checkbox"
-                              checked={checkedList?.has(report.reportCommentId)}
-                              onChange={() => {
-                                setCheckedList((prev) => {
-                                  const copy = new Set(prev);
-                                  if (copy.has(report.reportCommentId))
-                                    copy.delete(report.reportCommentId);
-                                  else copy.add(report.reportCommentId);
-                                  return copy;
-                                });
-                              }}
-                            />
-                            <div className="font-medium">
-                              {report.member.memberName}
-                            </div>
-                          </div>
-                          <div className="flex flex-row gap-2">
-                            <ReportTag category={report.category} />
-                            <IconButton
-                              Icon={
-                                <AiOutlineClose style={{ display: 'inline' }} />
-                              }
-                            />
-                          </div>
-                        </div>
-                        {idx % 7 === 0 && (
-                          <div className="mt-2 font-light">기타사유 detail</div>
-                        )}
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="flex justify-around p-2">
-                  <ButtonComponent type="outline">
-                    {`${checkedList?.size || 0}개의 신고 취소`}
-                  </ButtonComponent>
-                  <ButtonComponent onClick={handleRemoveBtnClick}>
-                    게시글 삭제
-                  </ButtonComponent>
-                </div>
-              </section>
-            </div>
-          </Modal>
-        )}
-        <div className={`p-4 gap-1.5 bg-box-bg border border-border relative`}>
-          <div className={'flex flex-row justify-between h-full w-full'}>
-            <div>
-              <span className={'font-bold text-sm truncate max-w-[200px]'}>
-                {title}
-              </span>
-              <div
-                className={'flex items-center justify-between w-full text-xs'}
-              >
-                <div>
-                  <BiTimeFive style={{ display: 'inline' }} />
-                  <span className={'text-[#9ca3af] text-xs text-right'}>
-                    {setDateWritten(date)}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="text-right absolute bottom-4 right-4">
-              <ButtonComponent onClick={handleClickViewButton}>
-                신고 사유 보기
-              </ButtonComponent>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  return <div>No Data</div>;
 }
