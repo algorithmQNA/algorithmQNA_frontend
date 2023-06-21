1869736599b4a55a8b3c5ea9d8c6e8ca9f69ea75
이진희 <49019236+bebusl@users.noreply.github.com>
2023-06-22 07:23:28 +0900

    Refactor: 유저 정보 들고오는 곳에 useGetMember훅 훅으로 대체

--- src/components/Board/PostView/PostOption/PostOptionBlock.tsx
+++ src/components/Board/PostView/PostOption/PostOptionBlock.tsx
@@ -1,32 +1,23 @@
-import PostDeleteButton from "./PostDeleteButton";
-import PostReportButton from "./PostReportButton";
+import PostDeleteButton from './PostDeleteButton';
+import PostReportButton from './PostReportButton';
 
-import {Link, useParams} from "react-router-dom";
-import useGetParams from "../../../../hooks/useGetParams";
-import {useQuery} from "react-query";
-import {getMemberDetailInfo} from "../../../../apis/authApi";
+import { Link, useParams } from 'react-router-dom';
+import useGetMember from '../../../../hooks/useGetMember';
 
-export default function PostViewOptionBlock({user_id}:{user_id:number}){
-    const {pid} = useParams()
-    const {data,isLoading} = useQuery(['member'],()=>getMemberDetailInfo())
-    if(isLoading){
-        return <div></div>
-    }
-    const member:any = data?.data as any
-    const memberId = member.data.memberId
-    return(
-        <div className={'w-full flex justify-end gap-4 text-content'}>
-            {
-                memberId === user_id &&
-                <Link to={`/post/update?pid=${pid}`}>
-                    수정
-                </Link>
-            }
-            {
-                memberId === user_id &&
-                <PostDeleteButton/>
-            }
-            <PostReportButton/>
-        </div>
-    )
+export default function PostViewOptionBlock({ user_id }: { user_id: number }) {
+  const { pid } = useParams();
+  const {data, isLoading} = useGetMember()
+
+  if (isLoading) {
+    return <div></div>;
+  }
+  const member: any = data?.data as any;
+  const memberId = member.data.memberId;
+  return (
+    <div className={'w-full flex justify-end gap-4 text-content'}>
+      {memberId === user_id && <Link to={`/post/update?pid=${pid}`}>수정</Link>}
+      {memberId === user_id && <PostDeleteButton />}
+      <PostReportButton />
+    </div>
+  );
 }
--- src/components/Header/re/User/UserMenu.tsx
+++ src/components/Header/re/User/UserMenu.tsx
@@ -1,71 +1,93 @@
-import React, {ChangeEvent, useEffect, useRef, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
-import {useQuery} from "react-query";
-import {getMemberDetailInfo} from "../../../../apis/authApi";
-import Rounded from "../../../RoundedImage/RoundedImage";
-import useCheckAuthority from "../../../../hooks/useCheckAuthority";
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Rounded from '../../../RoundedImage/RoundedImage';
+import useCheckAuthority from '../../../../hooks/useCheckAuthority';
+import useGetMember from '../../../../hooks/useGetMember';
 
-export default function UserMenuBlock(){
-    const {data,isLoading}:{data:any,isLoading:boolean} = useQuery(['user'],getMemberDetailInfo)
-    const isAdmin = useCheckAuthority();
-    const [state, setState] = useState({
-        menu: false,
-    })
-    const setDisplayAlarm = (e: ChangeEvent<HTMLInputElement>) => {
+export default function UserMenuBlock() {
+  const { data, isLoading } = useGetMember();
+  const isAdmin = useCheckAuthority();
+  const [state, setState] = useState({
+    menu: false,
+  });
+  const setDisplayAlarm = (e: ChangeEvent<HTMLInputElement>) => {
+    setState((prev) => ({
+      ...prev,
+      menu: e.target.checked,
+    }));
+  };
+  const box = useRef<any>(null);
+  useEffect(() => {
+    const setCheck = (e: globalThis.MouseEvent) => {
+      const target = e.target as Element;
+      if (state.menu && !box.current?.contains(target)) {
         setState((prev) => ({
-            ...prev,
-            menu: e.target.checked
+          ...prev,
+          menu: false,
         }));
+      }
     };
-    const box = useRef<any>(null)
-    useEffect(() => {
-        const setCheck = (e: globalThis.MouseEvent) => {
-            const target = e.target as Element;
-            if (state.menu && !box.current?.contains(target)) {
-                setState((prev) => ({
-                    ...prev,
-                    menu: false,
-                }));
+    document.addEventListener('click', setCheck);
+    return () => document.removeEventListener('click', setCheck);
+  }, [state.menu]);
+  return (
+    <div ref={box}>
+      <label
+        className={
+          'block w-[45px] h-[45px] rounded-full relative hover:cursor-pointer overflow-hidden'
+        }
+      >
+        <input
+          type={'checkbox'}
+          className={'hidden'}
+          checked={state.menu}
+          onChange={setDisplayAlarm}
+        />
+        <span className={''}>
+          {!isLoading && data && (
+            <Rounded
+              size={'sm'}
+              alt="프로필 이미지"
+              src={data.data.data.memberProfileUrl}
+            />
+          )}
+        </span>
+      </label>
+      {state.menu && (
+        <div
+          className={
+            'alarm absolute border border-primary w-[100px] right-0 top-[110%] bg-white  rounded shadow p-2 overflow-auto grid max-h-[250px] box-content'
+          }
+        >
+          {isAdmin && (
+            <Link
+              to={'/admin'}
+              className={
+                'text-content hover:text-primary text-sm flex items-center py-1'
+              }
+            >
+              관리자 페이지
+            </Link>
+          )}
+
+          <Link
+            to={'/mypage/profile'}
+            className={
+              'text-content hover:text-primary text-sm flex items-center py-1'
             }
-        };
-        document.addEventListener('click', setCheck);
-        return () => document.removeEventListener('click', setCheck);
-    }, [state.menu]);
-    return(
-        <div ref={box}>
-            <label className={'block w-[45px] h-[45px] rounded-full relative hover:cursor-pointer overflow-hidden'}>
-                <input
-                    type={'checkbox'}
-                    className={'hidden'}
-                    checked={state.menu}
-                    onChange={setDisplayAlarm}
-                />
-                <span className={''}>
-                    {
-                        !isLoading && data &&
-                        <Rounded size={'sm'} alt="프로필 이미지" src={data.data.data.memberProfileUrl}/>
-                    }
-                </span>
-            </label>
-            {state.menu && (
-                <div
-                    className={
-                        'alarm absolute border border-primary w-[100px] right-0 top-[110%] bg-white  rounded shadow p-2 overflow-auto grid max-h-[250px] box-content'
-                    }
-                >
-                    {isAdmin && 
-                    <Link to={'/admin'} className={'text-content hover:text-primary text-sm flex items-center py-1'}>
-                        관리자 페이지
-                    </Link>}
-                    
-                        <Link to={'/mypage/profile'} className={'text-content hover:text-primary text-sm flex items-center py-1'}>
-                        마이 페이지
-                    </Link>
-                    <Link to={'/logout'} className={'text-content hover:text-primary text-sm flex items-center py-1'}>
-                        로그아웃
-                    </Link>
-                </div>
-            )}
+          >
+            마이 페이지
+          </Link>
+          <Link
+            to={'/logout'}
+            className={
+              'text-content hover:text-primary text-sm flex items-center py-1'
+            }
+          >
+            로그아웃
+          </Link>
         </div>
-    )
+      )}
+    </div>
+  );
 }
