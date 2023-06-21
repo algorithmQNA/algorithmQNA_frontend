ab7b2e44f97ca31f197e7770cc04f30fa407c37f
이진희 <49019236+bebusl@users.noreply.github.com>
2023-06-22 07:19:06 +0900

    Feat: 알람 확인/삭제 api 호출 함수 작성

--- src/apis/alarmApi.ts
+++ src/apis/alarmApi.ts
@@ -1,4 +1,27 @@
-import {privateRequest} from "./instance";
+import { AlarmKindType, AlarmType } from '../types/Alarm';
+import { privateRequest } from './instance';
 
-export const createPostRequest = ({}) =>
-    privateRequest.get('/alarm');
+export const createPostRequest = ({}) => privateRequest.get('/alarm');
+
+export const getAlarm = ({
+  recentAlarmId,
+  oldAlarmId,
+}: {
+  recentAlarmId?: number;
+  oldAlarmId?: number;
+}) =>
+  privateRequest.get<{ data: { size: number; alarms: AlarmType[] } }>(
+    '/alarm',
+    {
+      params: {
+        recentAlarmId,
+        oldAlarmId,
+      },
+    }
+  );
+
+export const checkAlarm = (alarmId: number) =>
+  privateRequest.patch(`/alarm/${alarmId}`);
+
+export const deleteAlarm = (alarmId: number) =>
+  privateRequest.delete(`/alarm/${alarmId}`);
