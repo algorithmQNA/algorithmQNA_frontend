181171d2407c781fd49fb555d97c21414fc590e8
이진희 <49019236+bebusl@users.noreply.github.com>
2023-07-11 20:35:28 +0900

    Feat: 채택 답변 댓글 캐싱

--- src/components/Comment/PinnedCommentList.tsx
+++ src/components/Comment/PinnedCommentList.tsx
@@ -4,6 +4,10 @@ import { useQuery, useQueryClient } from 'react-query';
 import { getHightlightCommentListRequest } from '../../apis/postApi';
 import CommentWrapper from './CommentWrapper';
 import CommentView from './CommentView/CommentView';
+import { AiOutlineClose } from 'react-icons/ai';
+import { useRecoilState } from 'recoil';
+import HighlightStatusAtom from '../../storage/Highlight/Highlight';
+import { GENERAL_COMMENT, PINNED_COMMENT } from '../../constants/CommentType';
 
 type PinnedCommentViewProps = {
   commentId: number;
@@ -13,7 +17,8 @@ type PinnedCommentViewProps = {
 function PinnedCommentList({ commentId, pid }: PinnedCommentViewProps) {
   const [show, setShow] = useState(false);
   const [runEndAnimation, setRunEndAnimation] = useState(false);
-
+  const [, setHighlightSetting] = useRecoilState(HighlightStatusAtom);
+  const queryClient = useQueryClient();
   const sectionRef = useRef<HTMLDivElement>(null);
 
   const handleClickViewBtn = () => {
@@ -23,34 +28,89 @@ function PinnedCommentList({ commentId, pid }: PinnedCommentViewProps) {
     } else alert('채택된 답글이 없습니다!');
   };
 
-  const { data, isLoading } = useQuery(
-    ['highlight', +pid, +commentId],
-    () => getHightlightCommentListRequest(+pid, +commentId),
+  const { data: commentList, isLoading } = useQuery(
+    ['pin', +pid, +commentId],
+    async () => {
+      const res = await getHightlightCommentListRequest(+pid, +commentId);
+      return res.data.data.commentList.reverse();
+    },
     {
-      enabled: show && commentId > -1 && show,
+      onSuccess: (data) => {
+        console.log('SUCCESS', data);
+        data.forEach((comment) => {
+          if (!!comment.childSize) {
+            queryClient.setQueryData(['reply', +comment.commentId], (prev) => {
+              return {
+                pages: [
+                  {
+                    result: [...(comment.childCommentList as [])],
+                    nextPage: 1,
+                    prevPage: -1,
+                    isLast: !comment.next,
+                    isFirst: !comment.prev,
+                  },
+                ],
+                pageParams: 0,
+              };
+            });
+
+            comment.childCommentList?.forEach((comment) => {
+              if (!!comment.childSize) {
+                queryClient.setQueryData(
+                  ['reply', +comment.commentId],
+                  (prev) => {
+                    return {
+                      pages: [
+                        {
+                          result: [...(comment.childCommentList as [])],
+                          nextPage: 1,
+                          prevPage: -1,
+                          isLast: false,
+                          isFirst: true,
+                        },
+                      ],
+                    };
+                  }
+                );
+              }
+            });
+          }
+        });
+        setHighlightSetting((prev) => ({
+          ...prev,
+          highlightingMode: true,
+          commentType: PINNED_COMMENT,
+        }));
+      },
+      enabled: show && commentId > -1,
     }
   );
 
-  const commentList = data?.data.data.commentList;
-
   useEffect(() => {
     const goToHightlight = () => {
-      const pinnedElement = document.getElementById(`${commentId}`);
-      if (pinnedElement) {
-        pinnedElement.scrollIntoView({ behavior: 'smooth' });
-        pinnedElement.style.backgroundColor = '#fffec9';
-      }
+      if (show && !isLoading) {
+        const pinnedElement = document.getElementById(`PIN-${commentId}`);
+        if (pinnedElement) {
+          pinnedElement.scrollIntoView({ behavior: 'smooth' });
+          pinnedElement.style.backgroundColor = '#fffec9';
+        }
 
-      setTimeout(() => {
-        if (pinnedElement) pinnedElement.style.backgroundColor = '';
-      }, 800);
+        setTimeout(() => {
+          if (pinnedElement) pinnedElement.style.backgroundColor = '';
+          setHighlightSetting((prev) => ({
+            ...prev,
+            highlightingMode: false,
+            commentType: GENERAL_COMMENT,
+          }));
+        }, 800);
+      }
     };
-    const timeoutId = setTimeout(goToHightlight, 200);
+    const timeoutId = setTimeout(goToHightlight, 300);
 
     return () => {
       clearTimeout(timeoutId);
     };
-  }, [isLoading, show]);
+  }, [isLoading, show, commentId]);
 
   return (
     <div>
@@ -63,7 +123,7 @@ function PinnedCommentList({ commentId, pid }: PinnedCommentViewProps) {
       )}
 
       <section
-        className={`h-screen w-full md:w-1/2 z-50 bg-white shadow-xl fixed top-0 right-0 duration-150 p-2 overflow-y-auto ${
+        className={`h-screen w-full md:w-3/4 lg:w-1/2  bg-white shadow-xl fixed top-0 right-0 duration-150 z-50s overflow-y-auto ${
           show ? 'translate-x-0' : 'translate-x-full'
         } `}
         onTransitionEnd={() => {
@@ -74,19 +134,19 @@ function PinnedCommentList({ commentId, pid }: PinnedCommentViewProps) {
         }}
         ref={sectionRef}
       >
-        <header className="sticky top-0 left-0">
-          <p>채택된 답변</p>
-          <button className="p-2 w-fit" onClick={() => setShow(false)}>
-            닫기
+        <div className="sticky flex justify-between items-center top-0 left-0 border-b p-2 z-[1000] bg-white">
+          <p className="font-semibold">채택된 답변</p>
+          <button className="w-fit" onClick={() => setShow(false)}>
+            <AiOutlineClose />
           </button>
-        </header>
-        <div>
+        </div>
+        <div className="p-2">
           {commentList?.map((comment) => (
             <CommentWrapper
               depth={comment.depth}
               key={`highlight-${comment.commentId}`}
             >
-              <CommentView {...comment} />
+              <CommentView {...comment} commentType="PIN" />
             </CommentWrapper>
           ))}
         </div>
